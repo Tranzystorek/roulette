2af600bc3040556743c5777d102a71d54e903930
Marcin Puc <marcin.e.puc@gmail.com>
2019-03-21 19:47:30 +0100

    Fix indentation

--- roulette.sh
+++ roulette.sh
@@ -1,19 +1,19 @@
 function _roulette () {
-  # to scare the user
-  trap '' INT
-  trap '' QUIT
-  trap '' TSTP
+    # to scare the user
+    trap '' INT
+    trap '' QUIT
+    trap '' TSTP
 
-  # 1/6 chance to trigger simulated "rm -rfv --no-preserve-root /"
-  if [[ "$((RANDOM % 6))" -ne 0 ]]; then
-    echo "click"
-  else
-    find / -depth \
+    # 1/6 chance to trigger simulated "rm -rfv --no-preserve-root /"
+    if [[ "$((RANDOM % 6))" -ne 0 ]]; then
+        echo "click"
+    else
+        find / -depth \
             -type d -printf "removed directory '%p'\n" \
             -o -printf "removed '%p'\n" 2> /dev/null
-  fi
+    fi
 }
 
 function roulette () {
-  sudo bash -c "$(declare -f _roulette); _roulette"
+    sudo bash -c "$(declare -f _roulette); _roulette"
 }
